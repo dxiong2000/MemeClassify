311215579e7f1526de06d00a38fdbf045a55d4ab
daniel xiong <dxiong2000@gmail.com>
2019-08-01 22:47:31 +0800

    updated generate.py

--- generate.py
+++ generate.py
@@ -2,3 +2,23 @@
 generate.py
 generate training data using images in /templates/
 """
+
+import numpy as np
+import PIL
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import cv2
+import os
+import argparse
+
+chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
+class GenerateMeme:
+
+    def __init__(self, batch_size):
+        # do stuff
+
+
+
