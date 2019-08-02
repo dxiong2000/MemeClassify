0cab8330bc018bf2c96ae6efd7130f6c7037e081
int.ximing.xiong <int.ximing.xiong@uaes.com>
2019-08-02 14:41:38 +0800

    added main.py, updated generate.py

--- generate.py
+++ generate.py
@@ -3,6 +3,10 @@ generate.py
 generate training data using images in /templates/
 """
 
+import PIL
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 import numpy as np
 import cv2
 import os
@@ -11,14 +15,20 @@ MEME_PATH = "templates"
 SCALE_SIZE = (500, 750)
 
 
-def generate():
+def generate_exemplars():
 
     meme_array = []
+
     for meme in os.listdir(MEME_PATH):
         img_array = cv2.imread(os.path.join(MEME_PATH, meme))
         scaled = cv2.resize(img_array, SCALE_SIZE)
 
         # stores scaled image and meme name without .jpg extension
-        meme_array.append([scaled, meme[:-4]])
+        meme_array.append({"img": scaled, "class": meme[:-4]})
 
     return meme_array
+
+
+def generate_test_data(batch_size):
+
+    return
---
+++ main.py
@@ -0,0 +1,43 @@
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from scipy import average
+from generate import generate_exemplars
+from generate import generate_test_data
+import cv2
+
+
+def distance(exemplar, input):
+    return mean_squared_error(exemplar, input)
+
+
+def one_nearest_neighbor(input):
+    """
+    :param input: meme to be classified
+    :return: list of distances of the meme to each class
+    """
+    dists = []
+    exemplars = generate_exemplars()
+
+    # convert input image into 2D array of intensities
+    input = average(input, -1)
+
+    for meme in exemplars:
+        # extract image and class from exemplar
+        img = meme["img"]
+        cls = meme["class"]
+        # convert exemplar image into 2D array of intensities
+        img = average(img, -1)
+        # calculate difference in intensities
+        d = distance(img, input)
+        dists.append((d, cls))
+
+    # sort by least distance and return list
+    dists.sort()
+    return dists
+
+
+img = cv2.imread("test1.jpg")
+img = cv2.resize(img, (500, 750))
+dists = one_nearest_neighbor(img)
+
+print(dists[0])
